74946efa11df6eb2babfcfe2f0539f2ec391f635
Pablo Cesar Casi <pabloccasi@gmail.com>
2026-05-21 10:55:51 -0300

    Agregados DTOs por Pablo

---
+++ ISFDyT124/DTO/UsuarioCrearDTO.cs
@@ -0,0 +1,6 @@
+public class UsuarioCrearDto
+{
+    public string? UsApellido { get; set; }
+    public string? UsNombre { get; set; }
+    public int RoId { get; set; }
+}
---
+++ ISFDyT124/DTO/UsuarioDetalleDto.cs
@@ -0,0 +1,7 @@
+public class UsuarioDetalleDto
+{
+    public int UsId { get; set; }
+    public string? UsApellido { get; set; }
+    public string? UsNombre { get; set; }
+    public string? RoDenominacion { get; set; }
+}
