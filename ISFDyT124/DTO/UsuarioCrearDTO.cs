3909a11fccb60eeeb96beecb1641ef64f1cc6511
Pablo Cesar Casi <pabloccasi@gmail.com>
2026-05-22 15:38:40 -0300

    Correcciones a DTOs 2

--- ISFDyT124/DTO/UsuarioCrearDTO.cs
+++ ISFDyT124/DTO/UsuarioCrearDTO.cs
@@ -1,10 +1,10 @@
-namespace ISFDyT124
+namespace ISFDyT124.DTO
 {
     public class UsuarioCrearDto
     {
         public string? UsApellido { get; set; }
         public string? UsNombre { get; set; }
+        public int UsDni { get; set; }
         public int RoId { get; set; }
     }
 }
-
