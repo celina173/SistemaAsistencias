70c6ed95430c27becf2a84054852d46d0184b43f
Faku <fakundo_alvarez@hotmail.com>
2026-05-27 20:51:37 -0300

    Enhance Carrera model with validation attributes

--- ISFDyT124/Models/Carrera.cs
+++ ISFDyT124/Models/Carrera.cs
@@ -5,13 +5,23 @@ namespace ISFDyT124.Models
     public class Carrera
     {
         [Key]
+        [Display(Name = "ID Carrera")]
         public int ca_id { get; set; }
 
-        [Required]
-        [StringLength(100)]
-        public string ca_denominacion { get; set; }
 
-        
-        public ICollection<CarrerasMaterias>? CarrerasMaterias { get; set; }
+       
+        [Required(ErrorMessage = "Debe ingresar una denominación para la carrera.")]
+        [StringLength(100, ErrorMessage = "No se permiten más de 100 caracteres.")]
+        [RegularExpression(
+            @"^[A-Za-zÁÉÍÓÚáéíóúÜüÑñ0-9\s.,()-]*$",
+            ErrorMessage = "Ingrese una denominación válida."
+        )]
+        [Display(Name = "Denominación")]
+        public string ca_denominacion { get; set; } = null!;
+
+
+
+        // RELACION
+        public virtual ICollection<CarrerasMaterias>? CarrerasMaterias { get; set; }
     }
 }
