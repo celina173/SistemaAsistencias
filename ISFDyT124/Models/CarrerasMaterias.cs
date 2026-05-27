3c6cfa8ae4530af7b793ddf2710c4a04f9f0f2d7
Faku <fakundo_alvarez@hotmail.com>
2026-05-27 20:57:41 -0300

    Add data annotations to CarrerasMaterias properties

--- ISFDyT124/Models/CarrerasMaterias.cs
+++ ISFDyT124/Models/CarrerasMaterias.cs
@@ -6,16 +6,28 @@ namespace ISFDyT124.Models
     public class CarrerasMaterias
     {
         [Key]
+        [Display(Name = "ID Relación")]
         public int id { get; set; }
 
+        
+        [Required(ErrorMessage = "Debe seleccionar una carrera.")]
         [ForeignKey("Carrera")]
+        [Display(Name = "Carrera")]
         public int ca_id { get; set; }
 
+
+
+    
+        [Required(ErrorMessage = "Debe seleccionar una materia.")]
         [ForeignKey("Materia")]
+        [Display(Name = "Materia")]
         public int ma_id { get; set; }
 
-        public Carrera? Carrera { get; set; }
 
-        public Materia? Materia { get; set; }
+      
+        public virtual Carrera? Carrera { get; set; }
+
+
+        public virtual Materia? Materia { get; set; }
     }
 }
