a3f5557d037761fc0b6139bdece2ef1cb901286c
Valenpdf <valenhoms@gmail.com>
2026-05-27 21:27:03 -0300

    Correción en el modelo CarrerasMaterias

--- ISFDyT124/Models/CarrerasMaterias.cs
+++ ISFDyT124/Models/CarrerasMaterias.cs
@@ -6,34 +6,21 @@ namespace ISFDyT124.Models
     public class CarrerasMaterias
     {
         [Key]
-<<<<<<< HEAD
-        public int CaMaId { get; set; }
-=======
         [Display(Name = "ID Relación")]
-        public int id { get; set; }
->>>>>>> 3c6cfa8ae4530af7b793ddf2710c4a04f9f0f2d7
-
+        public int CaMaId { get; set; }
         
+
+        [Display(Name = "Carrera")]
         [Required(ErrorMessage = "Debe seleccionar una carrera.")]
         [ForeignKey("Carrera")]
-<<<<<<< HEAD
         public int CaId { get; set; }
-=======
-        [Display(Name = "Carrera")]
-        public int ca_id { get; set; }
->>>>>>> 3c6cfa8ae4530af7b793ddf2710c4a04f9f0f2d7
 
-
-
-    
+        [Display(Name = "Materia")]
         [Required(ErrorMessage = "Debe seleccionar una materia.")]
         [ForeignKey("Materia")]
-<<<<<<< HEAD
         public int MaId { get; set; }
-=======
-        [Display(Name = "Materia")]
-        public int ma_id { get; set; }
->>>>>>> 3c6cfa8ae4530af7b793ddf2710c4a04f9f0f2d7
+        
+
 
 
       
