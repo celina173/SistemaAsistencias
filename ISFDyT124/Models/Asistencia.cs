c6a7ab4e08d69e536f5a8813edac05998dc24fa5
Ángel <ansansoni23@gmail.com>
2026-06-08 20:03:02 -0300

    Se agregaron los datos al modelo Asistencia

--- ISFDyT124/Models/Asistencia.cs
+++ ISFDyT124/Models/Asistencia.cs
@@ -1,6 +1,36 @@
-﻿namespace ISFDyT124.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ISFDyT124.Models
 {
     public class Asistencia
     {
+        // Identificador único del registro.
+        [Key]
+        public int AsId { get; set; }
+
+        // Fecha y hora exacta en la toma de asistencia.
+        [Required(ErrorMessage = "La fecha es obligatoria")]
+        [Display(Name = "Fecha y Hora")]
+        public DateTime? AsFecha { get; set; }
+
+        //Bloque horario de la clase.
+        [Display(Name = "Presente")]
+        public bool AsPresente { get; set; } = false;
+
+        // Motivo o justificación en caso de ausencia.
+        [Display(Name = "Justificación")]
+        public bool AsJustificacion { get; set; } = false;
+
+        // Clave foránea que conecta con el estudiante.
+        public int? UsId { get; set; } // Por el momento no se utiliza
+
+        // Clave foránea que conecta con la a materia
+        public int? MaId { get; set; }
+
+        // Conexión hacia el modelo Usuarios.
+        public virtual Usuario? Usuario { get; set; } // Por el momento no se utiliza
+
+        // Conexión hacia el modelo Materias.
+        public virtual Materia? Materias { get; set; } // Por el momento no se utiliza
     }
 }
