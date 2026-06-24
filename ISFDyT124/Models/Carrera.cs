5c8898a39fa3899b6449b662d7485bc0d403029d
fakundosamuel <fakundo_alvarez@hotmail.com>
2026-06-24 19:30:02 -0300

    modelos carreras-materias

---
+++ ISFDyT124/Models/Carrera.cs
@@ -0,0 +1,19 @@
+﻿namespace ISFDyT124.Models
+{
+    public class Carrera
+    {
+        [Key]
+        public int ca_id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string ca_denominacion { get; set; }
+
+        [Required]
+        [StringLength(20)]
+        public string ca_codigo { get; set; }
+
+        
+        public ICollection<CarrerasMaterias>? CarrerasMaterias { get; set; }
+    }
+}
---
+++ ISFDyT124/Models/CarrerasMaterias.cs
@@ -0,0 +1,18 @@
+﻿namespace ISFDyT124.Models
+{
+    public class CarrerasMaterias
+    {
+        [Key]
+        public int id { get; set; }
+
+        [ForeignKey("Carrera")]
+        public int ca_id { get; set; }
+
+        [ForeignKey("Materia")]
+        public int ma_id { get; set; }
+
+        public Carrera? Carrera { get; set; }
+
+        public Materia? Materia { get; set; }
+    }
+}
