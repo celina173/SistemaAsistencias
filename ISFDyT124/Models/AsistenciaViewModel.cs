78e6ecf95dc9267adcb5e6a718a231cbd68649e7
Ángel Sansoni <tenshineta@gmail.com>
2026-07-15 15:52:11 -0300

    Se arregló la vista Asistencia y AsistenciaGlobal, ahora en la segunda pantalla se pueden visualizar de forma correcta los % de los estudiantes en los días donde se tomaron asistencias. Se compiló la solución y no se encontraron nuevos errorres.

--- ISFDyT124/Models/AsistenciaViewModel.cs
+++ ISFDyT124/Models/AsistenciaViewModel.cs
@@ -8,14 +8,13 @@ namespace ISFDyT124.Models
         public string FullName { get; set; } = string.Empty;
         public bool AsPresente { get; set; }
         public bool AsJustificacion { get; set; }
-        public List<bool> Modulos { get; set; } = new List<bool> { false, false, false, false };
+        public List<string> Modulos { get; set; } = new List<string>();
     }
 
     public class AsistenciaFormViewModel
     {
-        public int? CaMaId { get; set; }
-        // Número de módulos que tiene la materia (se usa en el controlador)
         public int ModuleCount { get; set; } = 1;
+        public int? CaMaId { get; set; }
         public List<AsistenciaRowViewModel> Rows { get; set; } = new List<AsistenciaRowViewModel>();
     }
 }
